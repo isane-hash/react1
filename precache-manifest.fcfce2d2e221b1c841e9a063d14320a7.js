bbab97f01360636629abdd6d8415ef081270ae48
isane-hash <isanejossy@gmail.com>
2020-02-02 08:51:36 +0200

    Updates

---
+++ precache-manifest.fcfce2d2e221b1c841e9a063d14320a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021624a0fd57e37dd4dccfc79edd8191",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "872d6e599faf5f325d9f",
+    "url": "/react1/static/css/main.d603c25e.chunk.css"
+  },
+  {
+    "revision": "58b5780d3c1e1992fee0",
+    "url": "/react1/static/js/2.d87aa644.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react1/static/js/2.d87aa644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "872d6e599faf5f325d9f",
+    "url": "/react1/static/js/main.13ae8432.chunk.js"
+  },
+  {
+    "revision": "b01edcf93abc3b2a750c",
+    "url": "/react1/static/js/runtime-main.e268764c.js"
+  }
+]);
